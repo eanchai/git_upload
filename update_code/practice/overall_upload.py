fe70a73c34244ad29282cf18ee82f78202ab5f12
ec.kim@kr-geninus.com <ubuntu@ip-172-31-41-141.ap-northeast-2.compute.internal>
2022-09-29 02:17:07 +0000

    modify

--- update_code/practice/overall_upload.py
+++ update_code/practice/overall_upload.py
@@ -80,16 +80,16 @@ class OverallUpdate:
         tb_expr_seq_line_df = tb_expr_seq_line_df.rename(columns={'sample_id':'SampleID'})
         return tb_expr_seq_line_df
     
-    #call에서 query 날려서 common부분 filter 추가해서 merge생성
+    #call에서 query 날려서 common merge & filter
     def filter_merged_qc_df(self, qc_df: pd.DataFrame, tb_expr_seq_line_df: pd.DataFrame) -> pd.DataFrame:
         qc_df['SampleID'] = [f"'{x}'" for x in qc_df['SampleID']] 
-        qc_df['SampleID'] = [x.replace("'","") for x in qc_df['SampleID']]
+        #qc_df['SampleID'] = [x.replace("'","") for x in qc_df['SampleID']]
         qc_merged = pd.merge(qc_df, tb_expr_seq_line_df, how='left', on='SampleID')
         qc_merged = qc_merged.rename(columns={'SampleID':'SAMPLE_ID'})
         qc_merged['FILTER'] = ['PASS' if row['FASTQ_TOTAL_BASES(Gb)'] > row['data_output'] else 'FAIL' for _, row in qc_merged.iterrows()]
         return qc_merged
     
-    #merge에서 db table처럼 table 수정, filter_merged_qc_df값을 여기 아래 넣으면,, 될걸
+    #db table처럼 table 수정, call에서 qc_merged return 되는 filter_merged_qc_df 값 넣기
     def revise_df(self, qc_merged: pd.DataFrame) -> pd.DataFrame:
         #원래 connect_db랑 연결할 수 있는데 test에 올리니까..
         #revise_df(self, conn, qc_merged)해서 쓰면 될듯..?
@@ -113,8 +113,59 @@ class OverallUpdate:
             qc_merged[col] = None
         qc_merged['LAST_UPDATE_DATE'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         qc_merged = qc_merged[table_cols]
+    
+    #gh2 업로드 시 이렇게 쓰면 될듯
+    #def update_sql(self, conn, qc_merged: pd.DataFrame):
+    #    qc_merged.to_sql(name='gc_qc_bi', con = conn, if_exists='replace', index=False)
+    def update_sql(self, qc_merged: pd.DataFrame):
+        db_type = 'mariadb'
+        id = 'root'
+        pw =  'gw12341234'
+        host = 'db-qc.cg10utv5zpvf.ap-northeast-2.rds.amazonaws.com'
+        schema_name = 'geninus'
+        url = f'{db_type}+pymysql://{id}:{pw}@{host}/{schema_name}'
+        engine = create_engine(url)
+        meta = MetaData(bind=engine)
+        MetaData.reflect(meta)
+        con = engine.connect()
         
-        def update_sql(self, conn, qc_merged: pd.DataFrame):
-            qc_merged.to_sql(name='gc_qc_bi', con = conn, if_exists='replace', index=False)
+        qc_merged.to_sql(name='gc_qc_bi', con = con, if_exists='replace', index=False)
+
 
 def __call__(self, qc_path: Path):
+    self.logger.info("Start fastqc_overall update pipeline")
+    self.logger.info("Connect db and parsing the data")
+     #DB connection initialize
+    conn = self.connect_db()
+
+    # load QC df
+    qc_df = pd.read_csv(qc_path)
+        
+    # load tb_expr_seq_line from database
+    query = f'''
+        select sample_id, data_output from tb_expr_seq_line
+        where sample_id in ({",".join(qc_df['SampleID'].tolist())})
+        '''
+    tb_expr_seq_line_df = self.parse_db_to_df(conn, query)
+    
+    self.logger.info("Filtering Data with threshold...")
+    qc_merged = self.filter_merged_qc_df(qc_df, tb_expr_seq_line_df)
+    
+    self.logger.info("Revise the table form...")
+    qc_merged = self.revise_df(qc_merged)
+    
+    self.logger.info("Update test database...")
+    try:
+        self.update_sql(qc_merged)
+    except Exception as e:
+        self.logger.info("Error!")
+        self.logger.info(e)
+        
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_dir", '-d')
+    parser.add_argument("--sample_id", '-sid')
+    args = parser.parse_args()
+    runner = OverallUpdate(args.data_dir, args.sample_id)
+    runner()
